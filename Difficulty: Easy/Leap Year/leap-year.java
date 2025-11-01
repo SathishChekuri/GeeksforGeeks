7af81a0a88645d96a50c72e337bada2f70d549ef
SathishChekuri <chekurisathish41@gmail.com>
2025-11-01 22:20:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Leap Year/leap-year.java
@@ -0,0 +1,13 @@
+//Back-end complete function Template for Java
+import java.util.*;
+
+public class Solution {
+    public static void solve() {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        if((year%400==0)||(year%4==0&&year%100!=0)) System.out.print("True");
+        else System.out.println("False");
+        // code here
+        
+    }
+}
