39b4f053b34551a6b0fc8bd239fe39bfc6c159bc
SathishChekuri <chekurisathish41@gmail.com>
2025-11-02 12:25:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Even Odd Game/even-odd-game.java
@@ -0,0 +1,11 @@
+// User function Template for Java
+class Solution {
+    public static String utility(int n) {
+
+        // write your code here
+        String r="";
+        if(n%2==0) r+="Friend";
+        else r+="You";
+        return r;
+    }
+}
