5f65a88c7284e3334edfe4dfd3ffcedffcc6d98d
SathishChekuri <chekurisathish41@gmail.com>
2025-11-06 09:34:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Divisor/divisor.java
@@ -0,0 +1,11 @@
+// User function Template for Java
+class Solution {
+    public static void divisor(int n) {
+
+        // Write your code here
+        for(int i=1;i<n;i++){
+            if(n%i==0) System.out.print(i+" ");
+        }
+        System.out.print(n);
+    }
+}
