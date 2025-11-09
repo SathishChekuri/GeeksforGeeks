42c28515aa0f500d5300842a6666fbbc5f4065c7
SathishChekuri <chekurisathish41@gmail.com>
2025-11-09 10:31:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/LCM/lcm.java
+++ Difficulty: Basic/LCM/lcm.java
@@ -1,15 +1,16 @@
 // User function Template for Java
 class Solution {
     public static int LCM(int a, int b) {
-
+         int prd=a*b;
+         while(b>0){
+             int t=a%b;
+             a=b;
+             b=t;
+         }
+         return prd/a;
         // write your code here
-        int prd=a*b;
-        while(b>0){
-            int t=a%b;
-            a=b;
-            b=t;
-        }
-        return prd/a;
+        
+
         // return LCM of a and b
     }
 }
