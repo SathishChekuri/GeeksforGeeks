5b3dffe5a415c60ae273ac552da8388d1b14638e
SathishChekuri <chekurisathish41@gmail.com>
2025-11-14 11:46:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/The Else Statement/the-else-statement.java
+++ Difficulty: Basic/The Else Statement/the-else-statement.java
@@ -1,11 +1,10 @@
-import java.util.*;
-
-public class Solution {
-    public static void main(String args[]) {
-        // Your Code Here
-        Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        if(a<=100) System.out.print("Number");
-        else System.out.print("Big");
+// User function Template for Java
+class Solution {
+    public static void utility(int number) {
+        // Write your if-else statement below
+        if(number>100) System.out.println("Big");
+       
+        // Write your if-else statement above
+         else System.out.println("Number");
     }
 }
