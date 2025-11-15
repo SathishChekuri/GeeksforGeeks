7ad6c5791c08f32fb16a2b4a9c07d38fd7238c8c
SathishChekuri <chekurisathish41@gmail.com>
2025-11-15 20:43:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Bitwise Operators/bitwise-operators.java
+++ Difficulty: Basic/Bitwise Operators/bitwise-operators.java
@@ -1,13 +1,6 @@
-//Back-end complete function Template for Java
-import java.util.Scanner;
-
-public class Solution {
-    public static void solve() {
-        Scanner scn = new Scanner(System.in);
-        int a = scn.nextInt();
-        int b = scn.nextInt();
-        int c = scn.nextInt();
-        System.out.println((int)(a^a)+" "+(int)(~(c^b))+" "+(int)(a&b)+" "+(int)(c|(a^a)));
-        // Perform all the operations and print in a single line
+class Solution {
+    public static void operators(int a, int b, int c) {
+        // code here
+        System.out.println((a^a)+" "+(~(c^b))+" "+(a&b)+" "+(c|(a^a)));
     }
 }
