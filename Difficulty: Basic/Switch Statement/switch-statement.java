b598eec84ff0f35f9e271e90ddb551f236d36e47
SathishChekuri <chekurisathish41@gmail.com>
2025-11-17 21:18:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Switch Statement/switch-statement.java
+++ Difficulty: Basic/Switch Statement/switch-statement.java
@@ -1,20 +1,28 @@
-// User function Template for Java
 class Solution {
-    public static String utility(int number) {
-
-        // write your code here
-        switch(number){
-            case 1:
-                return "One";
-            case 2: return "Two";
-            case 3: return "Three";
-            case 4: return "Four";
-            case 5: return "Five";
-            case 6: return "Six";
-            case 7: return "Seven";
-            case 8: return "Eight";
-            case 9: return "Nine";
-            default : return "Unknown";
+    public void solve() {
+        // code here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        switch(n){
+            case 1:System.out.print("One");
+            break;
+            case 2:System.out.print("Two");
+            break;
+            case 3:System.out.print("Three");
+            break;
+            case 4:System.out.print("Four");
+            break;
+            case 5:System.out.print("Five");
+            break;
+            case 6:System.out.print("Six");
+            break;
+            case 7:System.out.print("Seven");
+            break;
+            case 8:System.out.print("Eight");
+            break;
+            case 9:System.out.print("Nine");
+            break;
+            default: System.out.print("Unknown");
         }
     }
 }
