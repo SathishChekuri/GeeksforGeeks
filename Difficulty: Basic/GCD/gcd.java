2e37167846203408a930d8b0d27ce783d5cd309b
SathishChekuri <chekurisathish41@gmail.com>
2025-11-17 21:37:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/GCD/gcd.java
+++ Difficulty: Basic/GCD/gcd.java
@@ -1,12 +1,11 @@
-// User function Template for Java
 class Solution {
-    public static int gcd(int a, int b) {
-         while(b!=0){
-             int t=b;
-             b=a%b;
-             a=t;
-         }
-         return a;
-        // code here to calculate and return gcd of a and b
+    public int computeGCD(int a, int b) {
+        // code here
+        while(b!=0){
+            int t=b;
+            b=a%b;
+            a=t;
+        }
+        return a;
     }
 }
