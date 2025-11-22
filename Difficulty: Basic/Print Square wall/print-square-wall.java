edafc6ddee5e567228228d0d05985e618844b360
SathishChekuri <chekurisathish41@gmail.com>
2025-11-22 20:52:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Print Square wall/print-square-wall.java
+++ Difficulty: Basic/Print Square wall/print-square-wall.java
@@ -1,14 +1,10 @@
-import java.util.*;
-
-public class Solution {
-    public static void main(String args[]) {
-        // Your Code Here
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        for(int i=1;i<=n;i++){
-            System.out.print("*");
-            for(int j=1;j<n;j++){
-                System.out.print(" *");
+// User function Template for Java
+class Solution {
+    public static void squareWall(int s) {
+        // Write your code here
+        for(int i=0;i<s;i++){
+            for(int j=0;j<s;j++){
+                System.out.print("* ");
             }
             System.out.println();
         }
