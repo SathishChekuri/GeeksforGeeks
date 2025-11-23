4eaa51b5501ab5583a3da293f8775e5003a566df
SathishChekuri <chekurisathish41@gmail.com>
2025-11-23 08:02:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Inverted Right AngleTriangle/inverted-right-angle-triangle.java
+++ Difficulty: Basic/Inverted Right AngleTriangle/inverted-right-angle-triangle.java
@@ -1,8 +1,10 @@
+// User function Template for Java
 class Solution {
-    public void printPattern(int n) {
-        // Code here
-        for(int i=n;i>0;i--){
-            for(int j=i;j>0;j--){
+    public static void invTriangleWall(int s) {
+
+        // Write your code here
+        for(int i=0;i<s;i++){
+            for(int j=s;j>i;j--){
                 System.out.print("* ");
             }
             System.out.println();
