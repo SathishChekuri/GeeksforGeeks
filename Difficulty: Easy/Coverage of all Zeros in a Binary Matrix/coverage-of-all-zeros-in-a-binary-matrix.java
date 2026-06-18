16f0f6b90de150041ece590e3f942250d70130bc
SathishChekuri <chekurisathish41@gmail.com>
2026-06-18 08:16:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Coverage of all Zeros in a Binary Matrix/coverage-of-all-zeros-in-a-binary-matrix.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int findCoverage(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                if (mat[i][j] == 1) continue;
+
+                int cov = 0;
+
+                // left
+                for (int k = j - 1; k >= 0; k--) {
+                    if (mat[i][k] == 1) {
+                        cov++;
+                        break;
+                    }
+                }
+
+                // right
+                for (int k = j + 1; k < m; k++) {
+                    if (mat[i][k] == 1) {
+                        cov++;
+                        break;
+                    }
+                }
+
+                // up
+                for (int k = i - 1; k >= 0; k--) {
+                    if (mat[k][j] == 1) {
+                        cov++;
+                        break;
+                    }
+                }
+
+                // down
+                for (int k = i + 1; k < n; k++) {
+                    if (mat[k][j] == 1) {
+                        cov++;
+                        break;
+                    }
+                }
+
+                ans += cov;
+            }
+        }
+
+        return ans;
+    }
+}
