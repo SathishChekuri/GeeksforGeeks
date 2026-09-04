7e288cb9dce6107292fff49b0bd607a39957349d
SathishChekuri <chekurisathish41@gmail.com>
2026-09-04 08:41:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
@@ -1,39 +1,45 @@
 class Solution {
     public int spanningTree(int V, int[][] edges) {
         // code here
-        ArrayList<ArrayList<Pair>> ar=new ArrayList<>();
+        int parent[]=new int[V];
+        int rank[]=new int[V];
         for(int i=0;i<V;i++){
-            ar.add(new ArrayList<>());
+           parent[i]=i;
+           rank[i]=0;
         }
         int cost=0;
+         ArrayList<Pair> ar=new ArrayList<>();
         for(int arr[]:edges){
-            ar.get(arr[0]).add(new Pair(arr[1],arr[2]));
-            ar.get(arr[1]).add(new Pair(arr[0],arr[2]));
+            ar.add(new Pair(arr[0],arr[1],arr[2]));
         }
-        boolean visited[]=new boolean [V];
-        PriorityQueue<Pair> pr=new PriorityQueue<>((x,y)->Integer.compare(x.wt,y.wt));
-        pr.add(new Pair(0,0));
-        while(!pr.isEmpty()){
-            Pair curr=pr.poll();
-            int node=curr.node;
-            int wt=curr.wt;
-            if(visited[node]) continue;
-             cost+=wt;
-            visited[node]=true;
-            for(Pair p:ar.get(node)){
-                if(!visited[p.node]){
-                    pr.add(new Pair(p.node,p.wt));
-                }
+        ar.sort((x,y)->Integer.compare(x.wt,y.wt));
+        for(Pair p:ar){
+            int pa_u=find(p.u,parent);
+            int pa_v=find(p.v,parent);
+            if(pa_u==pa_v) continue;
+            cost+=p.wt;
+            if(rank[pa_u]>rank[pa_v]) parent[pa_v]=pa_u;
+            else if(rank[pa_v]>rank[pa_u]) parent[pa_u]=pa_v;
+            else{
+                rank[pa_u]++;
+                parent[pa_v]=pa_u;
             }
         }
         return cost;
     }
+    int find(int node,int [] parent){
+        if(node==parent[node]) return node;
+        parent[node]=find(parent[node],parent);
+        return parent[node];
+    }
 }
 class Pair{
-    int node;
+    int u;
+    int v;
     int wt;
-    Pair(int node,int wt){
-        this.node=node;
+    Pair(int u,int v,int wt){
+        this.u=u;
+        this.v=v;
         this.wt=wt;
     }
 }
